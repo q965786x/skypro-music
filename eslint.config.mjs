7e999a650376c96af16be1876d7e2d81138922f3
q965786x <kasatka74@yandex.ru>
2026-01-23 21:03:26 +0700

    Создан репозиторий skypro-music. Было произведено обновление Node.js. Была произведена настройка проекта. Были натроены линтер EsLint и форматер Prettier. В приложении были созданы файлы page.tsx с разметкой приложения. Разметка была преобразована из html в tsx. Стили были перенесены в файл page.module.css. Далее главная страница была декомпозирована на компоненты: Navigation.tsx, CenterBlock.tsx, SideBar.tsx, Bar.tsx. Стили также были разнесены по соответствующим файлам: Navigation.module.css, CenterBlock.module.css, SideBar.module.css, Bar.module.css. Теги img и <a></a> были заменены на <Image /> и <Link></Link>. Все изображения и конки хранятся в папке public.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,33 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import next from '@next/eslint-plugin-next';
+import typescriptParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@next/next': next,
+      '@typescript-eslint': typescriptEslintPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@next/next/no-html-link-for-pages': 'error',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+];
+
+
+{/* import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
@@ -15,4 +44,4 @@ const eslintConfig = defineConfig([
   ]),
 ]);
 
-export default eslintConfig;
+export default eslintConfig; */}
